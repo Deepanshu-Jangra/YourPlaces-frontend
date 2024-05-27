38fd91f162654a086949fee586a4db909880cf76
Deepanshu <deepanshujangra1224@gmail.com>
2024-05-27 11:39:29 +0530

    updated

--- src/user/components/AuthForm.js
+++ src/user/components/AuthForm.js
@@ -84,7 +84,7 @@ const AuthForm = () => {
     if (isLoginMode) {
       try {
         const responseData = await sendRequest(
-          "http://localhost:5000/api/users/login",
+          `${process.env.REACT_APP_BACKEND_URL}/users/login`,
           "POST",
           JSON.stringify({
             email: formState.inputs.email.value,
@@ -108,7 +108,7 @@ const AuthForm = () => {
         formData.append("image", formState.inputs.image.value);
 
         const responseData = await sendRequest(
-          "http://localhost:5000/api/users/signup",
+          `${process.env.REACT_APP_BACKEND_URL}/users/signup`,
           "POST",
           formData
         );
